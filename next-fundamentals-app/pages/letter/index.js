a1412416ef342b0172c113f8e456e16a851ddc9a
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2023-04-19 19:42:11 +0400

    Linking Between Pages

--- next-fundamentals-app/pages/letter/index.js
+++ next-fundamentals-app/pages/letter/index.js
@@ -1,7 +1,22 @@
-import React from "react";
+import Link from "next/link";
 
 const LetterIndex = () => {
-	return <h1>index file in Letter folder</h1>;
+	return (
+		<>
+			<h1>index file in Letter folder</h1>
+			<ul>
+				<li>
+					<Link href="/letter/letter">Letter Page</Link>
+				</li>
+				<li>
+					<Link href="/news">News Page</Link>
+				</li>
+				<li>
+					<Link href="/">Home Page</Link>
+				</li>
+			</ul>
+		</>
+	);
 };
 
 export default LetterIndex;
