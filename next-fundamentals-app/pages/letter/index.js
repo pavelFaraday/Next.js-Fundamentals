b85ddd1c570e17c8ec5ee3e3bf05e062c0c409a3
Giorgi Epitashvili <50376580+pavelFaraday@users.noreply.github.com>
2023-04-19 18:15:06 +0400

    Add Nested Paths & Pages (Nested Routes)

---
+++ next-fundamentals-app/pages/letter/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LetterIndex = () => {
+	return <h1>index file in Letter folder</h1>;
+};
+
+export default LetterIndex;
---
+++ next-fundamentals-app/pages/letter/letter.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LetterPage = () => {
+	return <h1>Letter Subpage in letter folder </h1>;
+};
+
+export default LetterPage;
